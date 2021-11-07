c8eb3e9f0b41ee671e51045ec1965b97bac6259e
Tian Xu <xueqingxu0302@gmail.com>
2021-11-07 16:23:08 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+
+const H1 = document.createElement("h1");
+document.body.append(H1);
+
+
+H1.setAttribute("id","victory");
+
+const newHeader = "Skylar is the champion ."
+H1.innerHTML = newHeader;
+
